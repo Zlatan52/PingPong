a11180a32009b63f55e8865cb708cb79587b916c
Zlatn52 <neymar01041986@mail.ru>
2025-04-27 17:29:07 +0300

    init

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from pygame import *
 from random import randint
  
- 
+font.init()
+
 WIDTH = 600
 HEIGHT = 500
 FPS = 60
@@ -13,7 +14,19 @@ BACKGROUND_COLOR = (randint(0, 255), randint(0, 255), randint(0, 255))
 WHITE = (255, 255, 255)
 GREEN = (0, 150, 0)
 RED = (150, 0, 0)
- 
+
+font_score = font.Font(None, 50)
+font_text = font.Font(None, 36)
+
+lose1 = font_text.render('Player 1 Lose', True, RED)
+lose2 = font_text.render('Player 2 Lose', True, RED)
+win1 = font_text.render('Player 1 Win', True, GREEN)
+win2 = font_text.render('Player 2 Win', True, GREEN)
+
+score1 = 0
+score2 = 0
+
+
 window = display.set_mode((WIDTH, HEIGHT))
 display.set_caption("Ping-Ping Yoy")
 clock = time.Clock()
@@ -42,10 +55,21 @@ class Player(GameSprite):
             self.rect.y -= self.speed
         if keys[self.down] and self.rect.y < HEIGHT - self.rect.height:
             self.rect.y += self.speed
+
+class Ball(GameSprite):
+    def __init__(self, img, x, y, w, h, dx, dy):
+        super().__init__(img, x, y, w, h)
+        self.dx = dx
+        self.dy = dy
+
+    def update(self):
+        self.rect.x += self.dx
+        self.rect.y += self.dy
  
 racket1 = Player(K_w, K_s, "racket.png", 30, 200, 50, 150, 4)
 racket2 = Player(K_UP, K_DOWN, "racket.png", 520, 200, 50, 150, 4)   
- 
+ball = Ball('tenis_ball.png', 200, 200, 50, 50, 3,3)
+
 run = True
 finish = False
  
@@ -59,9 +83,51 @@ while run:
         # window.blit(background, (0, 0))
         racket1.reset()
         racket2.reset()
+        ball.reset()
  
         racket1.update()
         racket2.update()
+        ball.update()
+
+        if ball.rect.y > HEIGHT - 50 or ball.rect.y < 0:
+            ball.dy *= -1
+
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            ball.dx *= -1
+
+        if ball.rect.x < 0:
+            score2 += 1
+            ball.rect.x = 200
+            ball.rect.y = 200
+
+        if ball.rect.x > WIDTH:
+            score1 += 1
+            ball.rect.x = 200
+            ball.rect.y = 200
+
+        if score1 >= WIN_SCORE or score2 >= WIN_SCORE:
+            finish = True
+            if score1 > score2:
+                window.blit(win1, (200, 200))
+                window.blit(lose2, (200, 250))
+            else:
+                window.blit(win2, (200, 200))
+                window.blit(lose1, (200, 250))
+
+        score_text = str(score1) + ':' + str(score2)
+        score_img = font_score.render(score_text, True, WHITE)
+        score_rect = score_img.get_rect(center=(WIDTH // 2, 50))
+        window.blit(score_img, score_rect)
+
+    else:
+        score1 = 0
+        score2 = 0
+        finish = False
+        ball.rect.x = 200
+        ball.rect.y = 200
+        ball.dx = 3
+        ball.dy = 3
+        time.delay(RESTART_TIME)
  
     display.update()
     clock.tick(FPS)
